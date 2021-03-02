4ecdd8f97c70c02b77c3d82a8232fc5984feb847
Pranay-Raju <Pranay-Raju@users.noreply.github.com>
2021-03-02 17:15:08 +0530

    inistial Springboot commit

---
+++ SpringBoot/MinutesOfMeeting/src/test/java/com/example/MinutesOfMeeting/MinutesOfMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MinutesOfMeeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinutesOfMeetingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
