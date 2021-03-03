bbe071a4df82b24300a00380c8ac16903f7f7906
Pranay-Raju <Pranay-Raju@users.noreply.github.com>
2021-03-03 21:38:34 +0530

    login commit

---
+++ Angular/MoM/src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user: any;
+  constructor() { 
+    this.user = {loginId:'', password:''};
+
+  }
+
+  ngOnInit(): void {
+  }
+  
+
+  submitLoginForm(loginForm: any): void {
+    console.log("LoginForm Object Data : ");
+    console.log(loginForm);
+  }
+
+}
