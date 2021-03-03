c8e9cc9250e07cf5c35156f4cc40ec472ac579f7
E.Navya <navyaemmalla3456@gmail.com>
2021-03-03 13:41:54 +0530

    hello

---
+++ SpringBoot/MinutesOfMeeting/src/main/java/com/dto/loginCredentials.java
@@ -0,0 +1,20 @@
+package com.dto;
+
+import org.springframework.web.bind.annotation.PostMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class loginCredentials {
+	private String loginId;
+	private String password;
+
+}
